48345770729e90c308606399ebd2adea22c7499c
Marco Francke <marcofrancke@googlemail.com>
2012-02-13 06:25:35 +0100

    some major bugfixes and the begin of a clean version

--- script.js
+++ script.js
@@ -24,7 +24,6 @@
       currentReviews = $('#customerReviews'),
       currentStars = $('.tiny .crAvgStars'),
       tabmenue = [],
-      count = 0,
       data = {
         uk : {
           name: 'United Kingdom',
@@ -72,31 +71,30 @@
       return;
     }
 
-    var appendReviewsAndRatings = function (content) {
-      var reviews   = $(this).find('#customerReviews'),
-        stars       = $(this).find('.tiny .crAvgStars'),
+    var appendReviewsAndRatings = function (content, id) {
+      var reviews   = $(content).find('#customerReviews'),
+        stars       = $(content).find('.tiny .crAvgStars'),
         reviewsHtml = (reviews.length > 0) ? reviews.html() : '',
         starsHtml   = (stars.length > 0) ? stars.html() : '';
 
       // this country has not a review for this product
-      if (!reviews || !reviewsHtml || !stars || !starsHtml) {
-        _hideFaildCountryTab(this.id.substr(0, 2));
-
-        // clean up
-        $(this).remove();
+      if (!reviewsHtml || !starsHtml) {
         return;
       }
 
-      currentReviews.after('<div class="' + this.id.substr(0, 2) + '_review reviews" style="display: none">' + reviewsHtml + '</div>');
+      $('#tabmenue').append('<li><a id="' + id + '" href="javascript:;">' + data[id].name + '</a></li>');
+
+      currentReviews.after('<div class="' + id + '_review reviews" style="display: none">' + reviewsHtml + '</div>');
       
       if (starsHtml) {
-        currentStars.after('<p><span class="crAvgStars">' + starsHtml + '</span> (' + data[this.id.substr(0, 2)].name + ')</p>');
+        currentStars.after('<p><span class="crAvgStars">' + starsHtml + '</span> (' + data[id].name + ')</p>');
       }
-      // clean up
-      $(this).remove();
     };
 
     tabmenue.push('<ul id="tabmenue">');
+    tabmenue.push('</ul>');
+    tabmenue.push('<div class="shadow"></div>');
+    currentReviews.before(tabmenue.join(''));
     
     for (id in selectedCountries) {
       if (selectedCountries.hasOwnProperty(id)) {
@@ -107,33 +105,33 @@
 
         // current amazon page 
         if (currentUrl.indexOf('.' + id + '/') !== -1) {
-          tabmenue.push('<li><a id="' + id + '" class="current" href="javascript:;">' + data[id].name + '</a></li>');
+          $('#tabmenue').append('<li><a id="' + id + '" class="current" href="javascript:;">' + data[id].name + '</a></li>');
           currentReviews.addClass(id + '_review');
           continue;
         }
 
-        var helperId = id + '_' + count;
-        tabmenue.push('<li><a id="' + id + '" href="javascript:;">' + data[id].name + '</a></li>');
-        
-        // ADD A HELPER CONTAINER TO PUT THE CONTENT OF LOADED AMAZON-PAGES
-        $('body').append('<div style="display: none" id="' + helperId + '"></div>');
-
         // IF FOREIGN AMAZON-PAGE IS LOADED THAN GET THE "REVIEWS"
-        $('#' + helperId).load(data[id].url, appendReviewsAndRatings);
-        
-        count++;
+        (function (id) {
+          $.get(data[id].url, function (content, status) {
+            if (status === 'error') {
+              return;
+            }
+            appendReviewsAndRatings(content, id)
+          });
+        }(id));
       }
     }
-
-    tabmenue.push('</ul>');
-    tabmenue.push('<div class="shadow"></div>');
-    
-    currentReviews.before(tabmenue.join(''));
 
     // TABMENU CLICK LISTENER
     $('#tabmenue').live('click', function (event) {
       var a = $(event.target).closest('a'),
-        id = a[0].id;
+        id;
+        
+      if (a.length === 0) {
+        return;
+      }
+      
+      id = a[0].id;
 
       $('a.current').removeClass('current');
       a.addClass('current');
