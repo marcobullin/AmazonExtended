d60c4b2785ff1dbfcc3a8090ac6601cb9b748813
Marco Francke <marcofrancke@googlemail.com>
2012-02-13 07:09:55 +0100

    completing the new amazon extension

--- content.js
+++ content.js
@@ -65,17 +65,26 @@
             config,
 
             /**
-             * This method renders the tabmenu
+             * This method renders the reviews and ratings content
              */
             renderReviewsAndRatings = function () {},
 
+            /**
+             * This method renders the tab menu
+             */
             addTabMenu = function () {},
+
+            /**
+             * This method adds a tab item to the tab menu
+             */
+            addTabMenuItem = function () {},
 
             /**
              * This method renders the customer ratings (stars)
              */
             renderStarsOverview = function () {};
 
+        // if we don't find what we are looking for -> exit
         if (-1 !== currentUrl.indexOf('/dp/')) {
             key = 'dp';
         } else if (-1 !== currentUrl.indexOf('/product/')) {
@@ -84,8 +93,16 @@
             return;
         }
 
+        // read user settings
         chrome.extension.sendRequest({action: "getCountryCode"}, $.proxy(this.init, this));
 
+        /**
+         * Adding the structure of the tab menu to amazon page.
+         *
+         * @param nodelist currentReviews
+         * 
+         * @return void
+         */
         addTabMenu = function (currentReviews) {
             currentReviews.before('<ul id="tabmenue"></ul><div class="shadow"></div>');
 
@@ -107,6 +124,66 @@
             });  
         };
 
+        /**
+         * Adding a tab menu item
+         *
+         * @param integer id - our country code (de, en, com, ...)
+         * @param string name - name of the country
+         * @param string _class - optinal a class to highlight current tab item
+         *
+         * @return void
+         */
+        addTabMenuItem = function (id, name, _class) {
+            $('#tabmenue').append('<li><a id="' + id + '" class="' + _class + '" href="javascript:;">' + name + '</a></li>');
+        };
+
+        /**
+         * Render reviews and ratings in the bottom of the amazon page
+         *
+         * @param nodelist currentReviews
+         * @param string content - amazon page of the foreign country
+         * @param integer id - our country code (de, en, com, ...)
+         *
+         * @return void
+         */
+        renderReviewsAndRatings = function (currentReviews, content, id) {
+            var reviews   = $(content).find('#customerReviews'),
+                reviewsHtml = (reviews.length > 0) ? reviews.html() : '';
+
+            // this country has not a review for this product
+            if (!reviewsHtml.trim()) {
+                return;
+            }
+
+            addTabMenuItem(id, data[id].name, '');
+          
+            currentReviews.after('<div class="' + id + '_review reviews" style="display: none">' + reviewsHtml + '</div>');
+        };
+
+        /**
+         * Render stars of foreign countries aside to the product photo
+         *
+         * @param nodelist currentStars
+         * @param string content - amazon page of the foreign country
+         * @param integer id - our country code (de, en, com, ...)
+         *
+         * @return void
+         */
+        renderStarsOverview = function (currentStars, content, id) {
+            var stars = $(content).find('.tiny .crAvgStars'),
+                starsHtml = (stars.length > 0) ? stars.html() : '';
+
+            // this country has not a star preview
+            if (!starsHtml.trim()) {
+                return;
+            }
+
+            currentStars.after('<p><span class="crAvgStars">' + starsHtml + '</span> (' + data[id].name + ')</p>');
+        };
+
+        /**
+         * INIT
+         */
         this.init = function (usersettings) {
             config = usersettings;
 
@@ -114,7 +191,8 @@
                 rest           = keyTokens[1],
                 asin           = rest.split('/')[0],
                 currentReviews = $('#customerReviews'),
-                currentStars   = $('.tiny .crAvgStars');
+                currentStars   = $('.tiny .crAvgStars'),
+                id;
 
             // no reviews or ratings -> exit
             if (currentReviews.length === 0 || currentStars.length === 0) {
@@ -122,6 +200,33 @@
             }
 
             addTabMenu(currentReviews);
+
+            for (id in config.countryCode) {
+                if (config.countryCode.hasOwnProperty(id)) {
+                    // user won't see this country except it is his own
+                    if (config.countryCode[id] === false && currentUrl.indexOf('.' + id + '/') === -1) {
+                        continue;
+                    }
+
+                    // add tab item for current amazon page 
+                    if (currentUrl.indexOf('.' + id + '/') !== -1) {
+                        addTabMenuItem(id, data[id].name, 'current');
+                        currentReviews.addClass(id + '_review');
+                        continue;
+                    }
+
+                    // IF FOREIGN AMAZON-PAGE IS LOADED THAN GET THE "REVIEWS"
+                    (function (id) {
+                        $.get(data[id].url, function (content, status) {
+                            if (status === 'error') {
+                                return;
+                            }
+                        
+                            renderReviewsAndRatings(currentReviews, content, id);
+                        });
+                    }(id));
+                }
+            }
         };
     };
 }());
